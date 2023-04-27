3673e01debe1246c5717b76be3bc506b21616baf
SanPhantom <1300690777@qq.com>
2023-04-27 17:27:02 +0800

    fix: update vercel

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import AutoImport from "unplugin-auto-import/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/",
   mode: "production",
   esbuild: {
     jsxInject: `import React from 'react'`,
